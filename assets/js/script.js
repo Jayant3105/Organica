17d4ad5dd63c06218df54768a9ea6b93f08057cb
Jayant Bhati <jayantbhati1188@gmail.com>
2025-05-20 14:41:41 +0530

    Header Section is completed with toggle js also

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+'use script';
+
+
+// navbar toggle
+
+const navOpenBtn=document.querySelector("[data-nav-open-btn]");
+const navbar= document.querySelector("[data-navbar]");
+const navCloseBtn= document.querySelector("[data-nav-close-btn]");
+
+const navElems= [navOpenBtn,navCloseBtn];
+
+for(let i=0;i<navElems.length;i++){
+    navElems[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+    });
+}
+
+
+
+//Search toggle
+
+const searchContainer= document.querySelector("[data-search-wrapper]");
+const searchBtn=document.querySelector("[data-search-btn]");
+
+searchBtn.addEventListener("click",function(){
+    searchContainer.classList.toggle("active");
+});
