cd0c1e9c2f5d83796d8f19cf43e01b719a875c31
Jayant Bhati <jayantbhati1188@gmail.com>
2025-05-29 12:44:34 +0530

    Css of footer is added and back to top button is completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,55 @@
-'use script';
-
+"use script";
 
 // navbar toggle
 
-const navOpenBtn=document.querySelector("[data-nav-open-btn]");
-const navbar= document.querySelector("[data-navbar]");
-const navCloseBtn= document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
-const navElems= [navOpenBtn,navCloseBtn];
+const navElems = [navOpenBtn, navCloseBtn];
 
-for(let i=0;i<navElems.length;i++){
-    navElems[i].addEventListener("click", function(){
-        navbar.classList.toggle("active");
-    });
+for (let i = 0; i < navElems.length; i++) {
+  navElems[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
 }
-
-
 
 //Search toggle
 
-const searchContainer= document.querySelector("[data-search-wrapper]");
-const searchBtn=document.querySelector("[data-search-btn]");
+const searchContainer = document.querySelector("[data-search-wrapper]");
+const searchBtn = document.querySelector("[data-search-btn]");
 
-searchBtn.addEventListener("click",function(){
-    searchContainer.classList.toggle("active");
+searchBtn.addEventListener("click", function () {
+  searchContainer.classList.toggle("active");
 });
-
-
 
 //wishlist and cart toggle
 
-const panelBtns= document.querySelectorAll("[data-panel-btn]");
-const sidePanels=document.querySelectorAll("[data-side-panel]");
+const panelBtns = document.querySelectorAll("[data-panel-btn]");
+const sidePanels = document.querySelectorAll("[data-side-panel]");
 
-for(let i=0; i<panelBtns.length;i++){
-    panelBtns[i].addEventListener("click",function(){
-        let clickedElementDataValue= this.dataset.panelBtn;
+for (let i = 0; i < panelBtns.length; i++) {
+  panelBtns[i].addEventListener("click", function () {
+    let clickedElementDataValue = this.dataset.panelBtn;
 
-        for(let i=0;i<sidePanels.length;i++){
-            if(clickedElementDataValue==sidePanels[i].dataset.sidePanel){
-                sidePanels[i].classList.toggle("active");
-            } else{
-                sidePanels[i].classList.remove("active");
-            }
-        }
-    });
+    for (let i = 0; i < sidePanels.length; i++) {
+      if (clickedElementDataValue == sidePanels[i].dataset.sidePanel) {
+        sidePanels[i].classList.toggle("active");
+      } else {
+        sidePanels[i].classList.remove("active");
+      }
+    }
+  });
 }
 
+// Back to top
+
+const backToBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    backToBtn.classList.add("active");
+  } else {
+    backToBtn.classList.remove("active");
+  }
+});
